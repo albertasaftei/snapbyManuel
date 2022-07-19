fd373b7e27d2ce416d171cdcf4db69701a9e823e
alburt <albert.asaftei00@gmail.com>
2022-07-20 00:50:26 +0200

    Header component + utils.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { HeaderContainer, LogoContainer, Logo } from "./styles";
+import {
+  HeaderContainer,
+  LogoContainer,
+  Logo,
+  Option,
+  OptionsContainer,
+} from "./styles";
 import logo from "../../icons/logo.png";
 
 function Header() {
   return (
     <HeaderContainer
       transition={{ delay: 0.2, duration: 0.5, type: "tween" }}
-      initial={{ x: "-100vw" }}
-      animate={{ x: 0 }}
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
     >
       <LogoContainer>
         <Logo src={logo} />
       </LogoContainer>
+      <OptionsContainer>
+        <Option>About</Option>
+        <Option>Portfolio</Option>
+        <Option>Contact</Option>
+      </OptionsContainer>
     </HeaderContainer>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,17 +1,36 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { HEADER_HEIGHT } from "../../utils";
 
 export const HeaderContainer = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
   background-color: #ffcc00;
-  height: 70px;
-  padding: 0 5%;
+  height: ${HEADER_HEIGHT};
 `;
 
 export const LogoContainer = styled.div`
   height: 100%;
+  padding: 0 5%;
 `;
 
 export const Logo = styled.img`
   height: 100%;
+`;
+
+export const OptionsContainer = styled.div`
+  display: flex;
+  padding: 0 5%;
+`;
+
+export const Option = styled.div`
+  font-size: 20px;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  &:not(:last-of-type) {
+    padding-right: 30px;
+  }
 `;
